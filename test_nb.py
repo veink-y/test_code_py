00e4494551a252de9c6c85a767e9dd749ba821cc
Hongfei Yang <hongfei.yang@compass.com>
2022-07-12 14:19:34 +0800

    Update test_nb.py

--- test_nb.py
+++ test_nb.py
@@ -1,2 +1,12 @@
 # Databricks notebook source
+
 print("hello world")
+
+# COMMAND ----------
+
+print("hello world command 2")
+
+# COMMAND ----------
+
+if __name__ == "__main__":
+    print("main")
