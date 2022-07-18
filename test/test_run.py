64cf721ff1e27ba7d3596f6e4d28e9f715103ce5
Hongfei Yang <hongfei.yang@compass.com>
2022-07-18 11:27:17 +0800

    Update test_run.py

--- test/test_run.py
+++ test/test_run.py
@@ -8,12 +8,5 @@ print("test_run hello world command 2")
 
 # COMMAND ----------
 
-import json
-with open("test_json.json") as f:
-    test_json = json.loads(f.reads())
-    print(test_json)
-
-# COMMAND ----------
-
 if __name__ == "__main__":
     print("main test_run")
